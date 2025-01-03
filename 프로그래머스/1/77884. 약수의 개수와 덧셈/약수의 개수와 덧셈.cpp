8b278e348fc1b62a5772c58774fbdcf313cfd8d0
mandugorani <soonshin98@gmail.com>
2025-01-03 10:03:11 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 1.13 ms, Memory: 4.22 MB -BaekjoonHub

---
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.cpp
@@ -0,0 +1,26 @@
+#include <vector>
+using namespace std;
+
+int solution(int left, int right) {
+    int answer = 0;
+    for (int i= left;
+    i<= right;
+    i++){
+        int yaksu =0;
+        
+        for (int j=1;
+            j<=i;
+            j++){
+            if (i%j== 0){
+                yaksu++;
+            }
+        }
+        if (yaksu%2 == 0) {
+            answer += i;
+        }else{
+            answer -=i;
+        }
+        }
+    
+    return answer;
+}
