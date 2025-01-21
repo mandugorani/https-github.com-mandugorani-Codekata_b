931eb1f6dedf886bd0ae253e57f57426c23f1243
mandugorani <soonshin98@gmail.com>
2025-01-21 09:13:10 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 4.22 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(vector<int> array, vector<vector<int>> commands) {
+    vector<int>answer;
+    for (auto& command:commands) {
+        int i = command[0] -1;
+        int j = command[1];
+        int k = command[2] -1;
+        
+        vector<int>sliced(array.begin() +i,
+                         array.begin() +j);
+        sort(sliced.begin(), sliced.end());
+        answer.push_back(sliced[k]);
+    }
+return answer;
+}
